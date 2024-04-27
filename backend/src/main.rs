c96207117541bba8322f8c03a3a626b1dae7cb6b
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-04-27 10:25:58 +0200

    feat: add “hello world” for axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::extract::Path;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello World"
+}
+
+async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
+    format!("Hello {name}")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/:name", get(hello_name));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
