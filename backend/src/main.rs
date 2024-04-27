8d39f0507434a54028d7af2131828956697260e1
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-04-27 10:56:39 +0200

    feat: add echo RPC endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,17 @@
 use axum::extract::Path;
 use axum::response::IntoResponse;
 use axum::routing::get;
+use axum::routing::post;
+use axum::Json;
 use axum::Router;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Serialize, Deserialize)]
+struct Workshop {
+    num_attendees: i32,
+    people_like_it: bool,
+}
 
 async fn hello_world() -> impl IntoResponse {
     "Hello World"
@@ -11,11 +21,16 @@ async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
     format!("Hello {name}")
 }
 
+async fn workshop_echo(json: Json<Workshop>) -> impl IntoResponse {
+    json
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(hello_world))
-        .route("/:name", get(hello_name));
+        .route("/:name", get(hello_name))
+        .route("/echo", post(workshop_echo));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
