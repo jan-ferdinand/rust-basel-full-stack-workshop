66738edd3e26bcb05c738446d10f065da0757876
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-04-27 10:00:43 +0200

    chore: add config files for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: "all",
+    content: [
+        // include all rust, html and css files in the src directory
+        "./frontend/src/**/*.{rs,html,css}",
+        // include all html files in the output (dist) directory
+        "./frontend/dist/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            "cupcake",
+        ],
+    },
 }
-
